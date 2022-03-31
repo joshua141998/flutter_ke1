ec8a77ac1bb156f634e65a59f9b3eb57ec1e30ce
Joshua <joshuayoseph141@gmail.com>
2022-03-31 21:46:03 +0700

    commit operator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -62,4 +62,20 @@ void main() {
   print(kelas);
   print(kelas['nama']);
   print(kelas['kelas']);
+
+//operator
+  int a, b;
+  a = 9;
+  b = 7;
+  print("a = " + a.toString());
+  print("b = " + b.toString());
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
 }
