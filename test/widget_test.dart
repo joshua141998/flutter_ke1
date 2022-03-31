91fd2da397df1b12462b723f6ce9f484341c2e26
Joshua <joshuayoseph141@gmail.com>
2022-03-31 21:34:03 +0700

    commit tipe data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,61 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_ke1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  //tipe data dan variable
+//var
+  var mahasiswa = "Jejen";
+  var umur = 20;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  print(mahasiswa + " Umur - " + umur.toString());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //string
+  String mahasiswastring;
+  mahasiswastring = "Aan";
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  print(mahasiswastring);
+
+  //int
+  int semester;
+  semester = 6;
+
+  print(semester);
+
+  //double
+  double ipk;
+  ipk = 4.0;
+
+  print(ipk);
+
+  //boolean
+  bool benar = true;
+  bool salah = false;
+  bool tidakbenar = true;
+  bool tidaksalah = false;
+
+  //list
+  List<String> jurusan = [
+    "Teknik Informatika",
+    "Sistem Informasi",
+    "DKV",
+    semester.toString(),
+    ipk.toString()
+  ];
+
+  print(jurusan);
+
+  //map
+  Map<String, dynamic> kelas = {"nama": "beben", "kelas": "SI 6"};
+
+  print(kelas);
+  print(kelas['nama']);
+  print(kelas['kelas']);
 }
